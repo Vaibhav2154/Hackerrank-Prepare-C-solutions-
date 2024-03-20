6bb7440d497e0bdabb9653730e34a4b0df988d7d
Vaibhav M N <149036369+Vaibhav2154@users.noreply.github.com>
2024-03-20 12:04:01 +0530

    Create fivesum.c

---
+++ Loops and Conditionals/fivesum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n,s=0,i;
+    scanf("%d", &n);
+    while(n>0)
+    {
+    i=n%10;
+    s=s+i;
+    n=n/10;
+    }
+    
+    printf("%d",s);
+    return 0;
+}
