2afff83b18576b181ec3bb6eb5004a052514b338
Vaibhav M N <149036369+Vaibhav2154@users.noreply.github.com>
2024-03-20 12:06:38 +0530

    Update fivesum.c

--- Loops and Conditionals/fivesum.c
+++ Loops and Conditionals/fivesum.c
@@ -3,17 +3,16 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main() {
-	
-    int n,s=0,i;
-    scanf("%d", &n);
-    while(n>0)
-    {
-    i=n%10;
-    s=s+i;
-    n=n/10;
-    }
-    
-    printf("%d",s);
-    return 0;
+  int main() 
+{
+   int n,s=0,i;
+   scanf("%d", &n);
+   while(n>0)
+     {
+      i=n%10;
+      s=s+i;
+      n=n/10;
+     }
+   printf("%d",s);
+   return 0;
 }
